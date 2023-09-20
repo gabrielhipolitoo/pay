c4d3b9a98effcbdec4c1e3a0d88f36016d242b6e
gabrielhipoltioo <c.gabrielhipolitoh@gmail.com>
2023-09-20 07:44:31 -0300

    code review - change validation and add handleErrorMessege

--- src/Componentes/SingUp.js
+++ src/Componentes/SingUp.js
@@ -1,117 +1,115 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
-import { useAauthetication } from '../CustomHooks/useAuthentication';
+import { useAauthetication } from "../CustomHooks/useAuthentication";
 const SingUp = () => {
-    const [valueInputsObj, setValueInputsObj] = useState({});
+  const [valueInputsObj, setValueInputsObj] = useState({});
 
-    const { createUser, error, setError } = useAauthetication();
+  const { createUser, error, setError } = useAauthetication();
 
-   
-    function sendInforNewUser(e) {
-        e.preventDefault();
-        if (valueInputsObj?.name?.length <= 7) {
-            setError('Insira seu nome completo');
-        } else if (valueInputsObj?.email?.length <= 7) {
-            setError('Email invalido');
-        } else if (
-            valueInputsObj?.password != valueInputsObj?.passwordConfirm
-        ) {
-            setError('Senhas não estão iguais');
-        }
-        else{
-            createUser(valueInputsObj);
-        }
-        setError();
+  function sendInforNewUser(e) {
+    setError()
+    e.preventDefault();
 
+    const name = valueInputsObj?.name?.length <= 7;
+    const email = valueInputsObj?.email?.length <= 7;
+    const password = valueInputsObj?.password != valueInputsObj?.passwordConfirm;
+
+    if (name) {
+      setError("Insira seu nome completo");
+    } else if (email) {
+      setError("Email invalido");
+    } else if (password) {
+      setError("Senhas não estão iguais");
+    } else {
+      createUser(valueInputsObj);
     }
-    return (
-        <section className="container-sign-up">
-            <h1>Crie sua conta</h1>
-            <form className="container-sign-up-form">
-                <input
-                    style={
-                        JSON.stringify(error)?.includes('nome')
-                            ? { border: '1px solid #ff000094' }
-                            : {}
-                    }
-                    type="text"
-                    onBlur={handleValidationInput}
-                    onChange={(e) =>
-                        setValueInputsObj({
-                            ...valueInputsObj,
-                            [e.target.name]: e.target.value,
-                        })
-                    }
-                    name="name"
-                    id="name"
-                    placeholder="Nome completo"
-                />
-                <input
-                    style={
-                        JSON.stringify(error)?.includes('Email')
-                            ? { border: '1px solid #ff000094' }
-                            : {}
-                    }
-                    type="text"
-                    onBlur={handleValidationInput}
-                    onChange={(e) =>
-                        setValueInputsObj({
-                            ...valueInputsObj,
-                            [e.target.name]: e.target.value,
-                        })
-                    }
-                    name="email"
-                    id="email"
-                    placeholder="Email"
-                />
-                <input
-                    style={
-                        JSON.stringify(error)?.includes('Senhas')
-                            ? { border: '1px solid #ff000094' }
-                            : {}
-                    }
-                    type="password"
-                    // onBlur={handleValidationInput}
-                    onChange={(e) =>
-                        setValueInputsObj({
-                            ...valueInputsObj,
-                            [e.target.name]: e.target.value,
-                        })
-                    }
-                    name="password"
-                    id="password"
-                    placeholder="Senha"
-                />
-                <input
-                    style={
-                        JSON.stringify(error)?.includes('Senhas')
-                            ? { border: '1px solid #ff000094' }
-                            : {}
-                    }
-                    type="password"
-                    onBlur={handleValidationInput}
-                    onChange={(e) =>
-                        setValueInputsObj({
-                            ...valueInputsObj,
-                            [e.target.name]: e.target.value,
-                        })
-                    }
-                    name="passwordConfirm"
-                    id="passwordConfirm"
-                    placeholder="Confirme sua senha"
-                />
-                <span className="error-message">{error}</span>
-                <input
-                    type="submit"
-                    onClick={sendInforNewUser}
-                    name="criar"
-                    value="Criar Conta"
-                />
-            </form>
-            <Link to="/">Ja tem uma conta?</Link>
-        </section>
-    );
+
+  }
+  return (
+    <section className="container-sign-up">
+      <h1>Crie sua conta</h1>
+      <form className="container-sign-up-form">
+        <input
+          style={
+            JSON.stringify(error)?.includes("nome")
+              ? { border: "1px solid #ff000094" }
+              : {}
+          }
+          type="text"
+          onChange={(e) =>
+            setValueInputsObj({
+              ...valueInputsObj,
+              [e.target.name]: e.target.value,
+            })
+          }
+          name="name"
+          id="name"
+          placeholder="Nome completo"
+        />
+        <input
+          style={
+            JSON.stringify(error)?.includes("Email")
+              ? { border: "1px solid #ff000094" }
+              : {}
+          }
+          type="text"
+          onChange={(e) =>
+            setValueInputsObj({
+              ...valueInputsObj,
+              [e.target.name]: e.target.value,
+            })
+          }
+          name="email"
+          id="email"
+          placeholder="Email"
+        />
+        <input
+          style={
+            JSON.stringify(error)?.includes("Senhas")
+              ? { border: "1px solid #ff000094" }
+              : {}
+          }
+          type="password"
+          // onBlur={handleValidationInput}
+          onChange={(e) =>
+            setValueInputsObj({
+              ...valueInputsObj,
+              [e.target.name]: e.target.value,
+            })
+          }
+          name="password"
+          id="password"
+          placeholder="Senha"
+        />
+        <input
+          style={
+            JSON.stringify(error)?.includes("Senhas")
+              ? { border: "1px solid #ff000094" }
+              : {}
+          }
+          type="password"
+          onChange={(e) =>
+            setValueInputsObj({
+              ...valueInputsObj,
+              [e.target.name]: e.target.value,
+            })
+          }
+          name="passwordConfirm"
+          id="passwordConfirm"
+          placeholder="Confirme sua senha"
+        />
+        <span className="error-message">{error}</span>
+        <input
+          type="submit"
+          onClick={sendInforNewUser}
+          name="criar"
+          value="Criar Conta"
+        />
+      </form>
+      <Link to="/">Ja tem uma conta?</Link>
+    </section>
+  );
 };
 
 export default SingUp;
--- src/CustomHooks/useAuthentication.js
+++ src/CustomHooks/useAuthentication.js
@@ -1,63 +1,61 @@
-import { db } from '../firebase/config';
+import { auth } from "../firebase/config.js";
 import {
-    getAuth,
-    createUserWithEmailAndPassword,
-    signInWithEmailAndPassword,
-    updateProfile,
-    signOut,
-} from 'firebase/auth';
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  updateProfile,
+  signOut,
+} from "firebase/auth";
 
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
 export const useAauthetication = () => {
-    const [error, setError] = useState();
-    const [loading, setLoading] = useState(null);
-    const [cancelled, setCancelled] = useState(false);
+  const [error, setError] = useState();
+  const [loading, setLoading] = useState(null);
+  const [cancelled, setCancelled] = useState(false);
 
-    const auth = getAuth();
-
-    function checkIfIsCancelled() {
-        if (cancelled) {
-            return;
-        }
+  function checkIfIsCancelled() {
+    if (cancelled) {
+      return;
     }
+  }
 
-    const createUser = async (data) => {
-        checkIfIsCancelled();
-        setLoading(true);
-        try {
-            const { user } = await createUserWithEmailAndPassword(
-                auth,
-                data.email,
-                data.password
-            );
-
-            await updateProfile(user, {
-                displayName: data.name,
-            });
-        } catch (error) {
-            console.log(error.message);
-            if (error.message.includes('Password')) {
-                setError('A senha precisa no minimo 6 caracteres');
-                console.log('opa');
-            } else if (error.message.includes('/email-already-in-use')) {
-                setError('Email em uso');
-            } else if (error.meFFfssage.includes('missing-password')) {
-                setError('Digite as senhas');
-            }
-        }
-        setLoading(false);
+  function handleErrorMessege(error) {
+    const typeError = {
+      "auth/admin-restricted-operation": setError("Preencha os campos"),
+      "auth/missing-password": setError("Digite as senhas"),
+      "auth/email-already-in-use": setError("Email em uso"),
     };
+  }
 
-    useEffect(() => {
-        return () => setCancelled(true);
-    }, []);
-
-    return {
+  const createUser = async (data) => {
+    checkIfIsCancelled();
+    setLoading(true);
+    try {
+      const { user } = await createUserWithEmailAndPassword(
         auth,
-        createUser,
-        error,
-        setError,
-        loading,
-    };
+        data.email,
+        data.password
+      );
+
+      await updateProfile(user, {
+        displayName: data.name,
+      });
+    } catch (error) {
+      const ErrorMessege = error?.code;
+      handleErrorMessege(ErrorMessege);
+    }
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    return () => setCancelled(true);
+  }, []);
+
+  return {
+    auth,
+    createUser,
+    error,
+    setError,
+    loading,
+  };
 };
